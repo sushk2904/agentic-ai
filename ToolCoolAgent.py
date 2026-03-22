75b68288420c3c764a2edf86e1fab1d97a96ec9d
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-03-22 23:11:39 +0530

    Written the tool definition

--- ToolCoolAgent.py
+++ ToolCoolAgent.py
@@ -15,4 +15,43 @@ def search_tool(query: str):
     # mock search (replace with real API later)
     return f"Search results for '{query}': [Sample data]"
 
+#Tool Definition
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "calculator",
+            "description": "Solve math expressions",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "expression": {
+                        "type": "string",
+                        "description": "Math expression like 2+2"
+                    }
+                },
+                "required": ["expression"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "search_tool",
+            "description": "Search for information",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "query": {
+                        "type": "string",
+                        "description": "Search query"
+                    }
+                },
+                "required": ["query"]
+            }
+        }
+    }
+]
+
+
 
